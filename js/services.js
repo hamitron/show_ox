691b9a2f4c50349f92fc352dd54d113a9e7f9f7c
Hamilton Karl <hamilton@nonfatmedia.com>
2015-11-11 12:29:09 -0800

    git repo added to package.json, service cleanup

--- js/services.js
+++ js/services.js
@@ -1,13 +1,12 @@
 //https://docs.angularjs.org/api/ngResource/service/$resource
 var showfaxServices = angular.module('showfaxServices', ['ngResource']);
 
-showfaxServices.factory('projectList', ['$resource', function($resource){
-    return $resource('http://10.253.202.100:3000/api/v1/projects/');
-  }])
+showfaxServices
+	.factory('projectList', ['$resource', function($resource){
+    	return $resource('http://10.253.202.100:3000/api/v1/projects/');
+  	}])
   .factory('projectShow', ['$resource', function($resource){
-    return $resource('http://10.253.202.100:3000/api/v1/projects/:pid.json', {pid:'@pid'});
-  }]);
-
-// $http({method: 'GET', url: '/entity/'+id+'?access_token='+token, headers: {'Authorization': 'Bearer '+token}})
+    	return $resource('http://10.253.202.100:3000/api/v1/projects/:pid.json', {pid:'@pid'});
+  	}]);
 
 //, {}, {'get': {headers: {"Authorization": "Token token=nonfat"}}}
